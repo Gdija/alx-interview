a6970b9967597c10e704832c355884d7470666a6
Gdija <khadijaghadi00@gmail.com>
2024-03-22 05:45:11 -0700

    task

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+def minOperations(n):
+    """function to return the minimum operations of copy and paste"""
+    if n == 1:
+        return 0
+    for i in range(n//2, 0, -1):
+        """find the largest divisor"""
+        if n % i == 0:
+            return minOperations(i) + n//i
