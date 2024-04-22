1e75e6fc9136881a4989e290205c9a9437969439
Gdija <khadijaghadi00@gmail.com>
2024-04-21 17:05:16 -0700

    fetch data

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,1 +1,27 @@
 #!/usr/bin/node
+const request = require('request');
+
+function getMovieChar(movieId) {
+	const Url = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
+	request(Url, (error, response, body) => {
+		if (error) {
+			console.error('Error :', error);
+			return;
+		}
+		const jsonData = JSON.parse(body);
+		const characters = jsonData.characters;
+
+		characters.forEach((characterUrl) => {
+			request(characterUrl, (charError, charResponse, charBody) => {
+				if(charError) {
+					console.error('Error:',charError);
+					return;
+				}
+				const charData = JSON.parse(charBody);
+				console.log(`${charData.name}`);
+			});
+		});
+});
+}
+const movieId = process.argv[2];
+getMovieChar(movieId);
