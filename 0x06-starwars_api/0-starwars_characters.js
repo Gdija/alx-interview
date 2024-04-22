70e370f117838d2c70ceea1b95a71bcefa9ec9bd
Gdija <khadijaghadi00@gmail.com>
2024-04-21 18:17:51 -0700

    fix semistandar

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,34 +1,34 @@
 #!/usr/bin/node
 const request = require('request');
 
-function getMovieChar(movieId) {
-	const Url = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
-	request(Url, (error, response, body) => {
-		if (error) {
-			console.error('Error :', error);
-			return;
-		}
-		const jsonData = JSON.parse(body);
-		const characters = jsonData.characters;
-		let index = 0;
+function getMovieChar (movieId) {
+  const Url = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
+  request(Url, (error, response, body) => {
+    if (error) {
+      console.error('Error :', error);
+      return;
+    }
+    const jsonData = JSON.parse(body);
+    const characters = jsonData.characters;
+    const index = 0;
 
-		function charById(index) {
-			if (index >= characters.length){
-				return;
-			}
-			const characterUrl = characters[index];
-			request(characterUrl, (charError, charResponse, charBody) => {
-				if (charError) {
-					console.error('Error:', charError);
-				} else {
-					const charData = JSON.parse(charBody);
-					console.log(`${charData.name}`);
-					charById(index + 1);
-				}
-			});
-		}
-		charById(index)
-	});
+    function charById (index) {
+      if (index >= characters.length) {
+        return;
+      }
+      const characterUrl = characters[index];
+      request(characterUrl, (charError, charResponse, charBody) => {
+        if (charError) {
+          console.error('Error:', charError);
+        } else {
+          const charData = JSON.parse(charBody);
+          console.log(`${charData.name}`);
+          charById(index + 1);
+        }
+      });
+    }
+    charById(index);
+  });
 }
 const movieId = process.argv[2];
 getMovieChar(movieId);
