422e1855b414b6e458bf64b87b6f2331069699a1
Gdija <khadijaghadi00@gmail.com>
2024-05-03 00:53:48 +0100

    task

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -5,7 +5,7 @@ determine the fewest number of coins needed to meet a given amount total
 
 
 def makeChange(coins, total):
-
+    """ function to make changes"""
     if (total == 0):
         return 0
     elif (!total):
