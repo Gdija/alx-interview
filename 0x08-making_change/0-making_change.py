7d9a535ddae79193880a399e8a90f5de7960208a
Gdija <khadijaghadi00@gmail.com>
2024-05-03 10:53:43 +0100

    update file

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -5,7 +5,17 @@ determine the fewest number of coins needed to meet a given amount total
 
 
 def makeChange(coins, total):
-    """function to make changes"""
+    """"
+    Calculate the fewest number of coins needed to make change for the given total amount.
+
+    Parameters:
+        coins (list of int): A list of coin denominations.
+        total (int): The total amount for which to make change.
+
+    Returns:
+        int: The fewest number of coins needed to make change for the given total amount.
+            Returns -1 if it's not possible to make change for the total amount.
+    """
     if (total == 0):
         return 0
     elif (!total):
