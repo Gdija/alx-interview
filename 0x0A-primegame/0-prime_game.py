b19980c4b653c91c2f4afd7c6e4fbec99e5d821b
Gdija <khadijaghadi00@gmail.com>
2024-05-17 01:32:19 +0100

    prime num

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
-""" determine the winner of the prime number game
-using Sieve of Eratosthenes algo"""
+""" determine the winner of the prime number game """
 
 
 def isWinner(x, nums):
-    """winner function based on number of rounds and array of n"""
+    """ winner function based on number of rounds and array of n """
     while(x <= len(nums)):
         for n in range(n+1):
