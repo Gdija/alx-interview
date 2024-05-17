7bdc6e093d26879eae7f751fbb23587de4fc48df
Gdija <khadijaghadi00@gmail.com>
2024-05-17 02:08:02 +0100

    prime num

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
+
 """ determine the winner of the prime number game """
 
 
 def isWinner(x, nums):
 
     """
-    x is the number of rounds and nums is an array of n
-    Return: name of the player that won the most rounds
-    If the winner cannot be determined, return None
-    You can assume n and x will not be larger than 10000
+    winner function based on number of rounds and array of n
+    x  is the number of rounds
+    nums array
     """
     while(x <= len(nums)):
         for n in range(n+1):
